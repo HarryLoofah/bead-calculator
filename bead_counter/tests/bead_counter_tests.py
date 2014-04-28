0a409b0801bf8fffc808179c8d20f5040efa1b43
HarryLoofah <HarryLoofah@users.noreply.github.com>
2014-04-27 22:01:05 -0600

    Update bead_counter_tests.py
    Forgot to sync to the mac before editing code locally. Made changes on GH website.

--- bead_counter/tests/bead_counter_tests.py
+++ bead_counter/tests/bead_counter_tests.py
@@ -1,15 +1,38 @@
 import unittest
-from bead_counter.sanity_check import SanityCheck
+from bead_counter import beads
+from bead_counter.sanity_check import less_than_12
 
-class SanityCheckTests(unittest.TestCase): 
-            
-    def test_total_beads_is_an_int(self):
+class SanityCheckTests(unittest.TestCase):
+    def setUp(self):
+        """
+        setUp is called before every test
+        """
         pass
+    def tearDown(self):
+        """
+        tearDown is called at the end of every test
+        """
+        pass 
+            
+    def test_total_beads_is_not_an_int(self):
+        """
+        Tests to make sure raw input is a whole integer.
+        """
+        actual_bead_number = beads
+        expected = ValueError
+        self.assertEqual (actual_bead_number, expected)
+        #self.assertRaises(ValueError)
         
     def test_total_beads_is_more_than_twelve(self):
-        sanity = SanityCheck
-        result = sanity.less_than_12(14)
-        self.assertTrue(result)
+        """
+        Tests to make sure raw input is greater than 12.
+        """
+        sanity = less_than_12(14)
+        self.assertTrue(sanity)
         
-    def test_total_beads_is_divisible_by_six_and_nine(self):
+    def test_total_beads_is_not_divisible_by_six_or_nine(self):
         pass
+
+if __name__ == '__main__':
+    # run all TestCase's in this module
+    unittest.main()
