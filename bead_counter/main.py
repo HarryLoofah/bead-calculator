abebf9a34be4f9409f6018cba7c7a193bcbbbcf3
HarryLoofah <harryloofah@gmail.com>
2014-04-29 05:57:38 -0600

    create pretty_table, changes to main and b_c_test
    changed SanityCheck name to CheckBeadNum, added class ProcessBeadNum,
    started test driven work in bead_counter_test

--- bead_counter/main.py
+++ bead_counter/main.py
@@ -30,7 +30,7 @@ and short side design elements will be available.
 
 __version__ = "0.0.1"
 
-class SanityCheck(object):
+class CheckBeadNum(object):
     """
     Before running full code, check to see that the number entered 
     (total_beads), is greater than 12 and is divisible by 6 or 9.
@@ -46,11 +46,27 @@ class SanityCheck(object):
         if total_beads % 6 != 0 and total_beads % 9 != 0:
             print('Error. Please use a number that is divisible by 6 or 9')
             return
+            
+class ProcessBeadNum(object):
+    """
+    Returns long and short design element values, number of beads to start with
+    and number of beads to add on the first row of beads, and if the raw_input
+    bead number entered doesn't pass tests (if it is'nt mod 6, 9, or 12), then
+    ProcessBeadNum suggests a number of total beads to try instead.
+    
+    Suggestion is based on which option (either higher bead count or lower) 
+    gives the largest number of design element options. Both high and low
+    suggestions are returned if design element options are equal.
+    """
+    # Returns long and short side of design elements available depending on
+    # whether the number of beads entered in raw_input is mod 6, 9, 0r 12.
+    ls_vals(self, total_beads):
+        pass
     
 
 if __name__ == "__main__":
     beads = raw_input("Please specify number of beads: ").isdigit()
-    bead_count = SanityCheck()
+    bead_count = CheckBeadNum()
     bead_count.less_than_12(beads)
     bead_count.not_divisible_by(beads)
     
--- bead_counter/tests/bead_counter_tests.py
+++ bead_counter/tests/bead_counter_tests.py
@@ -1,5 +1,6 @@
 import unittest
-from bead_counter.main import SanityCheck
+from bead_counter.main import CheckBeadNum
+from bead_counter.main import ProcessBeadNum
 
 class SanityCheckTests(unittest.TestCase):
     def setUp(self):
@@ -24,6 +25,14 @@ class SanityCheckTests(unittest.TestCase):
         Tests to make sure raw input is divisible by 6 or 9
         """
         pass
+        
+    def test_ls_vals_modulo_method_returns_correct_result(self):
+        """
+        Tests to make sure ls_vals returns modulo findsing
+        """
+        process = ProcessBeadNum()
+        result = process.ls_vals(beads=12)
+        self.assertTrue(result % 6 == 0)
 
 if __name__ == '__main__':
     # run all TestCase's in this module
