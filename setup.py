e0b7528e6c78420c6d987db350401475b314200b
HarryLoofah <harryloofah@gmail.com>
2014-05-04 07:17:42 -0600

    Registered package BeadCalculator with pypi.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Bead Calculator',
+    'description': 'A script to calculate peyote stich design options.',
     'author': 'Greg Aitkenhead',
     'url': 'URL where project will be available.',
     'download_url': 'Where to download it.',
     'author_email': 'none',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['bead_counter'],
+    'packages': ['bead_calculator'],
     'scripts': [],
-    'name': 'Bead Calculator'
+    'name': 'BeadCalculator'
 }
 
 setup(**config)
